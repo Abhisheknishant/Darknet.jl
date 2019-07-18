09a67c2930711ffc88f235807835f77e5d5dd233
Ian Butterworth <i.r.butterworth@gmail.com>
2019-07-18 11:11:43 -0400

    allow for different build options

--- deps/build.jl
+++ deps/build.jl
@@ -12,12 +12,31 @@ products = Product[
 
 # Download binaries from hosted location
 bin_prefix = "https://github.com/ianshmean/bins/raw/master/3rdparty/Darknet"
-                    
-download_info = Dict(
-    #Linux(:x86_64)  => ("$bin_prefix/Darknet-AlexeyAB-YOLOv3-cuda10.1-cudnn7.5.0-Ubuntu18.04.tar.gz", "5ddad410540fac019e1996a4705075b259b4bc2dd13d220dc9bfd90496e64ed1"),
-    MacOS(:x86_64)  => ("$bin_prefix/darknet-AlexeyAB-YOLOv3-MacOS.10.14.3-CPU-only.tar.gz", "c9d79e1918c785149d39920608b4efb22fc910895ab6baf9aa5f7f43169a37fe"),
-    #MacOS(:x86_64)  => ("$bin_prefix/arcbasic.tar.gz", "06803eab8c89ec7c1bf39796ea448217614362b8a51a6c82eaf286be1574ba4d")
-)
+download_info = Dict()                 
+if ENV["DARKNET_GPU"] == 1
+    if ENV["DARKNET_CUDNN"] == 1
+        if ENV["DARKNET_CUDNN_HALF"] == 1    
+            #GPU CUDNN & CUDNN_HALF
+            download_info = Dict(
+                MacOS(:x86_64)  => ("$bin_prefix/darknet-AlexeyAB-YOLOv3-MacOS.10.14.3-[GPU-CUDNN-CUDNN_HALF].tar.gz", "177c43bf864910e5e5549ef77fd65d7899c73a40e564dbc010cccd31af3f5feb")                  
+            )
+        else
+            #GPU,CUDNN
+        end
+    else
+        #GPU
+    end
+else
+    if ENV["DARKNET_OPENMP"] == 1
+        #CPU, OPENMP
+    else
+        #CPU
+        download_info = Dict(
+            MacOS(:x86_64)  => ("$bin_prefix/darknet-AlexeyAB-YOLOv3-MacOS.10.14.3-[CPU].tar.gz", "c9d79e1918c785149d39920608b4efb22fc910895ab6baf9aa5f7f43169a37fe"),                     
+        )
+    end
+end
+
 # First, check to see if we're all satisfied
 @show satisfied(products[1]; verbose=true)
 if any(!satisfied(p; verbose=false) for p in products)
